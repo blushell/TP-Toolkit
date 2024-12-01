8f850407ccc9fb083781e85ed0b9a7d7786c86e7
blushell <micheal.jones89@gmail.com>
2024-12-01 07:49:13 -0500

    theme color browser storage

--- composables/useTheme.js
+++ composables/useTheme.js
@@ -1,11 +1,12 @@
 export function useTheme() {
-	const theme = ref('dark');
+	const theme = ref(localStorage.getItem('theme') || 'dark');
 	const themeIcon = computed(() =>
 		theme.value === 'light' ? 'mdi-weather-night' : 'mdi-weather-sunny'
 	);
 
 	const toggleTheme = () => {
 		theme.value = theme.value === 'light' ? 'dark' : 'light';
+		localStorage.setItem('theme', theme.value);
 	};
 
 	return { theme, themeIcon, toggleTheme };
